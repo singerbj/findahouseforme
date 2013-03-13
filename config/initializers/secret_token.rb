45f582aa9e952dbf1f5cd8e54044349211ad6cf5
Benjamin Singer <singerbj@uwec.edu>
2013-03-13 12:27:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findahouseforme::Application.config.secret_token = 'eb7ce7653b9bb15e74b0f8323e952b3f883fea44112edebb1343d4238a17e2a6f839ab5a1af70efe2f1720c5ab0bfc6850d96a7cf12f2d62dd7eb7106c6d201d'
